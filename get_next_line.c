a21844a4e6f9d6ae9b51ea84aa86c1ec6ed140e6
Cquintana001 <cquintana002@gmail.com>
2022-05-27 10:46:33 +0200

    1st commit 27052022

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: caquinta <caquinta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 17:32:15 by caquinta          #+#    #+#             */
-/*   Updated: 2022/05/26 12:49:25 by caquinta         ###   ########.fr       */
+/*   Updated: 2022/05/27 10:40:01 by caquinta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,58 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stddef.h>
-void  checkline(char *line2, char *line)
+#include <stdio.h>
+
+int checkline(char *array) // return index of any '\n' in the string, otherwise return 0
 {
     int z;
-    int y;
-    char *temp;
-    
-    z=0;
-    y=0;
-    while (line2[z])
-	{
-		if (line2[z] == '\n')
-		{
-			line[y] = line2[z];
-			line[y + 1] = '\0';
-            z++;
+    int check;
 
-            while(line2[z])
-            {
-                y = 0;
-                temp[y]=line2[z];
-                z++;
-                y++; 
-            }
-            y=0;
-            z=0;
-            while(temp[y])
-            {
-                line2[z]=temp[y];
-                y++;
-                z++;
-            } 
-			 break;
-		}
-		else
-		{
-			line[y] = line2[z];
-			z++;
-			y++;
-		}
-	}
+    z = 0;
+    check = 0;
+    while (array[z])
+    {
+        if (array[z] == '\n')
+            return (z);
+        z++;
+    }
+    return (0);
+}
+void setline(char *array, int index) // trim the string until reach the index
+{
+    int z;
+    char temp[42];
+
+    z = 0;
+    index++;
+    while (array[index])
+    {
+        temp[z] = array[index];
+        z++;
+        index++;
+    }
+    temp[z] = '\0';
+    z = 0;
+    while (temp[z])
+    {
+        array[z] = temp[z];
+        z++;
+    }
+    array[z] = '\0';
+}
+
+int cpline(char *temp, char *array)
+{
+    int x;
      
+    x=0;
+    while(temp[x])
+    {
+        array[x] = temp[x];
+        x++;
+    }
+    array[x]='\0';
+     return(x);
 }
 char *get_next_line(int fd)
 {
@@ -66,50 +77,36 @@ char *get_next_line(int fd)
 	int x;
 	int y;
 	int z;
+	int check;
 	ssize_t nr_bytes;
 	char *temp;
 	
 	x = 0;
 	y = 0;
 	z = 0;
+	check = checkline(line2);
+	printf("check = %d\n", check);
 	line = malloc(300);
-	 
-	 while (line2[z])
-	{
-		if (line2[z] == '\n')
+	if(check!=0) 
+    {
+		while(x<=check)
 		{
-			line[y] = line2[z];
-			 z++;
-			 while(line2[z])
-			 {
-				temp[x] = line2[z];
-				x++;
-				z++; 
-			 }
-			 x=0;
-			 z=0;
-			 while(temp[x])
-			 {
-				 line2[z]= temp[x];
-				 x++;
-				 z++;
-			 }
-			 line2[z+1] = '\0';
-			 x = 0;
-			 z=0;
-			 break;
+			line[x] = line2[x];
+			x++;
 		}
-		else
-		{
-			line[y] = line2[z];
-			z++;
-			y++;
-		}
-	} 
+		line[x] ='\0';
+        setline(line2, check); //<= AQUÍ ESTA EL PROBLEMA!!!!!!
+        return(line);
+    }
+    else
+	  y = cpline(line2, line);
+
 	while (line[y] != '\n')
 	{
 		x = 0;
 		nr_bytes = read(fd, buf, BUFFER_SIZE);
+		if(nr_bytes==0)
+			return(line);
 		buf[nr_bytes] = '\0';
 		while (buf[x])
 		{
@@ -152,10 +149,10 @@ int main()
 	x = 0;
 
 	fd = open("/Users/caquinta/Desktop/pruebagnl.txt", O_RDONLY);
-	 while(x<3)
+	 while(x<5)
 	 {
 		result = get_next_line(fd);
-		printf("La string devuelta es: %s", result);
+		printf("linea %d es %s",x,  result);
 		free(result);
 		x++;
 	 }
